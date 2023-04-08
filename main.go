1b7b1c56729ec14c428201d4a0e7076747bba721
pravytecy <pravyedtech@gmail.com>
2023-04-08 22:17:20 +0530

    [feat : impl calCulategrade]

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 )
 
 type Grade string
@@ -32,7 +33,7 @@ func main() {
 }
 
 func parseCSV(filePath string) []student {
-	//var student []student
+	var students []student
 	f, err := os.Open(filePath)
 	if err != nil {
 		log.Fatal(err)
@@ -45,16 +46,65 @@ func parseCSV(filePath string) []student {
 	if err != nil {
 		log.Fatal(err)
 	}
-	for key, val := range data {
-		fmt.Println(key, val)
-
+	for index, val := range data {
+		if index > 0 {
+			var Student student
+			for i, field := range val {
+				fmt.Println(i, field)
+				if i == 0 {
+					Student.firstName = field
+				} else if i == 1 {
+					Student.lastName = field
+				} else if i == 2 {
+					Student.university = field
+				} else if i == 3 {
+					Student.test1Score = StringToInt(field)
+				} else if i == 4 {
+					Student.test2Score = StringToInt(field)
+				} else if i == 5 {
+					Student.test3Score = StringToInt(field)
+				} else if i == 6 {
+					Student.test4Score = StringToInt(field)
+				}
+			}
+			students = append(students, Student)
+		}
 	}
+	calculateGrade(students)
+	return students
+}
 
-	return nil
+func StringToInt(field string) int {
+	value, err := strconv.Atoi(field)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return value
 }
 
 func calculateGrade(students []student) []studentStat {
-	return nil
+	var studentStats []studentStat
+	for _, st := range students {
+		var stStat studentStat
+		avg := float32(st.test1Score+st.test2Score+st.test3Score+st.test4Score) / 4
+		stStat.student = st
+		stStat.finalScore = avg
+		stStat.grade = GetGrade(avg)
+		studentStats = append(studentStats, stStat)
+	}
+	return studentStats
+}
+
+func GetGrade(score float32) Grade {
+	if score >= 70 {
+		return A
+	} else if score < 70 && score >= 50 {
+		return B
+	} else if score < 50 && score >= 35 {
+		return C
+	} else {
+		return F
+	}
 }
 
 func findOverallTopper(gradedStudents []studentStat) studentStat {
